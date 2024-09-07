e4f56b9319ce83df2bbb023283a876972dd7970b
Denys <68016905+Nesfow@users.noreply.github.com>
2024-09-07 19:49:24 +1000

    Dev denys (#6)
    new app
    
    * clearing space
    
    * feat: initital commit and Model classes

---
+++ AT1CoffeeShop/Models/Orders.cs
@@ -0,0 +1,8 @@
+﻿namespace AT1CoffeeShop.Models
+{
+    public class Orders
+    {
+        public int OrderId { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+    }
+}
