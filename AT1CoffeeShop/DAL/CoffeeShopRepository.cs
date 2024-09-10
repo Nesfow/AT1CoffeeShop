7a6440e76963d69265d63199d90f88b823074393
Dan <nesfow@gmail.com>
2024-09-10 23:58:26 +1000

    feat: adding "Change customer name" and "Add item to order" to the UpdateOrder() method

--- AT1CoffeeShop/DAL/CoffeeShopRepository.cs
+++ AT1CoffeeShop/DAL/CoffeeShopRepository.cs
@@ -1,6 +1,8 @@
+using AT1CoffeeShop.BLL;
 using AT1CoffeeShop.Models.BusinessModels;
 using AT1CoffeeShop.Models.DBModels;
 using System.Data.SqlClient;
+using System.Xml.Linq;
 
 
 namespace AT1CoffeeShop.DAL
@@ -87,7 +89,108 @@ namespace AT1CoffeeShop.DAL
 
         public void UpdateOrder()
         {
+            Console.WriteLine("Enter the Id of the order to update: \n");
+            int orderIdToUpdate = Convert.ToInt32(Console.ReadLine());
 
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1. Change customer name");
+                Console.WriteLine("2. Add item to order");
+                Console.WriteLine("3. Edit order item");
+                Console.WriteLine("4. Remove order item");
+                Console.WriteLine("5. Go back");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("New customer name: ");
+                        var newCustomerName = Console.ReadLine();
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                        {
+                            connection.Open();
+                            string updateQuery = "UPDATE Orders SET CustomerName = @NewCustomerName WHERE OrderId = @OrderId";
+                            using (SqlCommand command = new SqlCommand(updateQuery, connection))
+                            {
+                                command.Parameters.AddWithValue("@OrderId", orderIdToUpdate);
+                                command.Parameters.AddWithValue("@NewCustomerName", newCustomerName);
+                                command.ExecuteNonQuery();
+                                Console.WriteLine("Customer name was updated.");
+                            }
+                        }
+
+                        Console.WriteLine("Anything else?");
+                        Console.WriteLine("1. Yes");
+                        Console.WriteLine("2. No");
+                        if (Console.ReadLine() == "2")
+                        {
+                            exit = true;
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("What item would you like to add to the order: ");
+
+                        // Dynamically checking items that possible to add.
+                        string selectQuery = "SELECT ItemId, CoffeeName FROM Items";
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                        {
+                            connection.Open();
+
+                            using (SqlCommand command = new SqlCommand(selectQuery, connection))
+                            {
+                                using (SqlDataReader reader = command.ExecuteReader())
+                                {
+                                    while (reader.Read())
+                                    {
+                                        Console.WriteLine($"Id: {reader.GetInt32(0)}, Coffee name: {reader.GetString(1)}");
+                                    }
+                                }
+                            }
+                        }
+
+                        int itemToAdd = Convert.ToInt16(Console.ReadLine());
+                        Console.WriteLine("How many items?");
+                        int itemQty = Convert.ToInt16(Console.ReadLine());
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                        {
+                            connection.Open();
+
+                            string insertQuery = "INSERT INTO OrderItems VALUES (@OrderId, @ItemId, @ItemQty)";
+                            using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                            {
+                                command.Parameters.AddWithValue("@OrderId", orderIdToUpdate);
+                                command.Parameters.AddWithValue("@ItemId", itemToAdd);
+                                command.Parameters.AddWithValue("@ItemQty", itemQty);
+                                command.ExecuteNonQuery();
+                            }
+
+                            Console.WriteLine("Item added.");
+                        }
+
+                        Console.WriteLine("Anything else?");
+                        Console.WriteLine("1. Yes");
+                        Console.WriteLine("2. No");
+                        if (Console.ReadLine() == "2")
+                        {
+                            exit = true;
+                        }
+
+                        break;
+                    case "3":
+                        //coffeeShopManager.UpdateOrder();
+                        break;
+                    case "4":
+                        //coffeeShopManager.CancelOrder();
+                        break;
+                    case "5":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("I am sorry, but this option doesn't exist. Please try another value :)");
+                        break;
+                }
+
+            }
         }
 
         public void CancelOrder()
